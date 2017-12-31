00c5dd88c7f848d7ccd0f706086f45c6233cd873
miosz <milosz.wozniak@gmail.com>
2017-12-31 09:40:37 +0100

    added comment

--- src/com/isajdqz1/helloworld/Main.java
+++ src/com/isajdqz1/helloworld/Main.java
@@ -3,7 +3,7 @@ package com.isajdqz1.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-        String imie = "Milosz";
+        String imie = "Milosz"; // zmienna imie
         System.out.println("Hello " + imie); //Hello World
     }
 }
