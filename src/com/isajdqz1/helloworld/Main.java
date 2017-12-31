c33da94bca706c042a0d6c8b9dd3b20c508a5dd0
miosz <milosz.wozniak@gmail.com>
2017-12-31 09:32:20 +0100

    comment

--- src/com/isajdqz1/helloworld/Main.java
+++ src/com/isajdqz1/helloworld/Main.java
@@ -3,6 +3,6 @@ package com.isajdqz1.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //Hello World
     }
 }
