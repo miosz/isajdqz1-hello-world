8a7889b00e1bfed978f657e21efe8afccad8d9da
miosz <milosz.wozniak@gmail.com>
2018-01-05 19:54:54 +0100

    add ocena2

--- src/com/isajdqz1/helloworld/Main.java
+++ src/com/isajdqz1/helloworld/Main.java
@@ -6,14 +6,14 @@ public class Main {
         String imie = "Milosz"; // zmienna imie
         System.out.println("Hello " + imie); //Hello World
 
-        uczen uczen1 = new uczen("Imie1","Nazwisko1",3);
-        System.out.println(uczen1.imie + " " + uczen1.nazwisko + " ocena: " + uczen1.ocena);
+        uczen uczen1 = new uczen("Imie1","Nazwisko1",3, 4);
+        System.out.println(uczen1.imie + " " + uczen1.nazwisko + " ocena1: " + uczen1.ocena1 + " ocena2: " + uczen1.ocena2);
 
-        uczen uczen2 = new uczen("Imie2","Nazwisko2",2);
-        System.out.println(uczen2.imie + " " + uczen2.nazwisko + " ocena: " + uczen2.ocena);
+        uczen uczen2 = new uczen("Imie2","Nazwisko2",2,3);
+        System.out.println(uczen2.imie + " " + uczen2.nazwisko + " ocena1: " + uczen2.ocena1 + " ocena2: " + uczen2.ocena2);
 
-        uczen uczen3 = new uczen("Imie3","Nazwisko3",4);
-        System.out.println(uczen3.imie + " " + uczen3.nazwisko + " ocena: " + uczen3.ocena);
+        uczen uczen3 = new uczen("Imie3","Nazwisko3",4, 5);
+        System.out.println(uczen3.imie + " " + uczen3.nazwisko + " ocena1: " + uczen3.ocena1 + " ocena2: " + uczen3.ocena2);
 
     }
 
--- src/com/isajdqz1/helloworld/uczen.java
+++ src/com/isajdqz1/helloworld/uczen.java
@@ -3,13 +3,15 @@ package com.isajdqz1.helloworld;
 public class uczen {
     public String imie;
     public String nazwisko;
-    public int ocena;
+    public int ocena1;
+    public int ocena2;
 
 
-    public uczen(String imie, String nazwisko, int ocena) {
+    public uczen(String imie, String nazwisko, int ocena1, int ocena2) {
         this.imie = imie;
         this.nazwisko = nazwisko;
-        this.ocena = ocena;
+        this.ocena1 = ocena1;
+        this.ocena2 = ocena2;
     }
 
 }
