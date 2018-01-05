5fdee5ff27f6f0ada5fa0924559d24792bee4f27
miosz <milosz.wozniak@gmail.com>
2018-01-05 19:50:37 +0100

    manual add uczen1 uczen2 uczen3

--- src/com/isajdqz1/helloworld/Main.java
+++ src/com/isajdqz1/helloworld/Main.java
@@ -6,6 +6,15 @@ public class Main {
         String imie = "Milosz"; // zmienna imie
         System.out.println("Hello " + imie); //Hello World
 
+        uczen uczen1 = new uczen("Imie1","Nazwisko1",3);
+        System.out.println(uczen1.imie + " " + uczen1.nazwisko + " ocena: " + uczen1.ocena);
+
+        uczen uczen2 = new uczen("Imie2","Nazwisko2",2);
+        System.out.println(uczen2.imie + " " + uczen2.nazwisko + " ocena: " + uczen2.ocena);
+
+        uczen uczen3 = new uczen("Imie3","Nazwisko3",4);
+        System.out.println(uczen3.imie + " " + uczen3.nazwisko + " ocena: " + uczen3.ocena);
 
     }
+
 }
--- src/com/isajdqz1/helloworld/uczen.java
+++ src/com/isajdqz1/helloworld/uczen.java
@@ -3,5 +3,13 @@ package com.isajdqz1.helloworld;
 public class uczen {
     public String imie;
     public String nazwisko;
-    public int[] oceny;
+    public int ocena;
+
+
+    public uczen(String imie, String nazwisko, int ocena) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.ocena = ocena;
+    }
+
 }
