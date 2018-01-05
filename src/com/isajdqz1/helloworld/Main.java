e17bf60c7a497e7b1fa8d7d3cc461690b7efb9f1
miosz <milosz.wozniak@gmail.com>
2018-01-05 20:12:56 +0100

    add manual srednia

--- src/com/isajdqz1/helloworld/Main.java
+++ src/com/isajdqz1/helloworld/Main.java
@@ -6,14 +6,18 @@ public class Main {
         String imie = "Milosz"; // zmienna imie
         System.out.println("Hello " + imie); //Hello World
 
-        uczen uczen1 = new uczen("Imie1","Nazwisko1",3, 4);
+        uczen uczen1 = new uczen("Imie1","Nazwisko1",3.0, 4.0);
         System.out.println(uczen1.imie + " " + uczen1.nazwisko + " ocena1: " + uczen1.ocena1 + " ocena2: " + uczen1.ocena2);
 
-        uczen uczen2 = new uczen("Imie2","Nazwisko2",2,3);
+        uczen uczen2 = new uczen("Imie2","Nazwisko2",2.0,3.0);
         System.out.println(uczen2.imie + " " + uczen2.nazwisko + " ocena1: " + uczen2.ocena1 + " ocena2: " + uczen2.ocena2);
 
-        uczen uczen3 = new uczen("Imie3","Nazwisko3",4, 5);
+        uczen uczen3 = new uczen("Imie3","Nazwisko3",4.0, 5.0);
         System.out.println(uczen3.imie + " " + uczen3.nazwisko + " ocena1: " + uczen3.ocena1 + " ocena2: " + uczen3.ocena2);
+
+       uczen1.srednia();
+       uczen2.srednia();
+       uczen3.srednia();
 
     }
 
--- src/com/isajdqz1/helloworld/uczen.java
+++ src/com/isajdqz1/helloworld/uczen.java
@@ -3,15 +3,36 @@ package com.isajdqz1.helloworld;
 public class uczen {
     public String imie;
     public String nazwisko;
-    public int ocena1;
-    public int ocena2;
+    public double ocena1;
+    public double ocena2;
 
 
-    public uczen(String imie, String nazwisko, int ocena1, int ocena2) {
+    public uczen(String imie, String nazwisko, double ocena1, double ocena2) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.ocena1 = ocena1;
         this.ocena2 = ocena2;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public double getOcena1() {
+        return ocena1;
+    }
+
+    public double getOcena2() {
+        return ocena2;
+    }
+
+    public void srednia() {
+        double srednia = (this.ocena1 + this.ocena2)/2;
+        System.out.println(srednia);
     }
 
 }
